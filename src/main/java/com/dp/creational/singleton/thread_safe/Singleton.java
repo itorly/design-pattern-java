c3f6dfdcf4478ec594f5c4b2d79177e627d07aec
itorly <itorly@163.com>
2025-02-03 22:25:17 +0800

    Singleton Pattern: eager-load, thread safe

---
+++ src/main/java/com/dp/creational/singleton/thread_safe/Singleton.java
@@ -0,0 +1,50 @@
+package com.dp.creational.singleton.thread_safe;
+
+import java.util.concurrent.*;
+
+public class Singleton {
+
+    private static int index = 0;
+
+    private Singleton() {
+        index++;
+    }
+
+    private static class SingletonHelper {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
+
+    public void printIndex() {
+        System.out.println("index:" + index);
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                10,
+                10,
+                60,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(10),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy()
+        );
+
+        for (int i = 0; i < 50; i++) {
+            executor.execute(new MyRunnable());
+        }
+
+    }
+}
+
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        Singleton singleton = Singleton.getInstance();
+        singleton.printIndex();
+    }
+}
