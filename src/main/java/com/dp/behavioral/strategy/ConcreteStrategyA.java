3245a8ed5c0e66ab8c75ec96885098b482c44731
itorly <itorly@163.com>
2025-02-09 22:11:21 +0800

    add the @Override annotation

--- src/main/java/com/dp/behavioral/strategy/ConcreteStrategyA.java
+++ src/main/java/com/dp/behavioral/strategy/ConcreteStrategyA.java
@@ -1,6 +1,8 @@
 package com.dp.behavioral.strategy;
 
 public class ConcreteStrategyA implements IStrategy {
+
+    @Override
     public void strategyMethod() {
         System.out.println("The strategy method of Specific Strategy A has been accessed.");
     }
--- src/main/java/com/dp/behavioral/strategy/ConcreteStrategyB.java
+++ src/main/java/com/dp/behavioral/strategy/ConcreteStrategyB.java
@@ -1,6 +1,8 @@
 package com.dp.behavioral.strategy;
 
 public class ConcreteStrategyB implements IStrategy {
+
+    @Override
     public void strategyMethod() {
         System.out.println("The strategy method of Specific Strategy B has been accessed.");
     }
